fda03266938fd40f68f240f976e5be23b63dca61
AMARTYA149 <amartya970914@gmail.com>
2022-11-11 21:24:22 +0530

    advanced transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,37 @@ import { Component } from "@angular/core";
           transform: "translateX(100px)",
         })
       ),
+      transition("normal <=> highlighted", animate(300)),
+      // transition("highlighted => normal", animate(800)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({ "background-color": "red", transform: "tranlateX(0) scale(1)" })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0px) scale(0.5)",
+        })
+      ),
       transition("normal => highlighted", animate(300)),
       transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", animate(500)),
     ]),
   ],
 })
 export class AppComponent {
   state = "normal";
+  wildState = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAdd(item) {
@@ -38,5 +62,10 @@ export class AppComponent {
 
   onAnimate() {
     this.state = this.state == "highlighted" ? "normal" : "highlighted";
+    this.wildState = this.wildState == "highlighted" ? "normal" : "highlighted";
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
   }
 }
